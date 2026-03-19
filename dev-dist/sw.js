db0bc42ba32531d67f70e96678731ccdf79aa5be
krishnatanniru <94739268+krishnatanniru@users.noreply.github.com>
2026-03-19 10:44:51 +0530

    Include latest build artifacts in dev-dist

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f0hpq4u6hqs"
+    "revision": "0.pjmbsea6d8c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
